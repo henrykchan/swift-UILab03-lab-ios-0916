ef656b0ec2d2fc258070410e4b48ceaa2109cdc6
henrykchan <chan.henryk@gmail.com>
2016-09-22 18:52:44 -0400

    UILab03 Complete

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,6 +12,15 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var centerImage: UILabel!
+    
+    
+    @IBOutlet weak var topLeftImage: UILabel!
+    
+    
+    @IBOutlet weak var bottomRightImage: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -20,6 +29,31 @@ class ViewController: UIViewController {
     
     // TODO: IB actions and code to update UI
 
+    @IBAction func cloverFour(_ sender: AnyObject) {
+        
+        centerImage.text = "4"
+        topLeftImage.text = "4"
+        bottomRightImage.text = "4"
+    }
   
+    @IBAction func spadeThree(_ sender: AnyObject) {
+        
+        centerImage.text = "3"
+        topLeftImage.text = "3"
+        bottomRightImage.text = "3"
+    }
 
+    @IBAction func diamondEight(_ sender: AnyObject) {
+        
+        centerImage.text = "8"
+        topLeftImage.text = "8"
+        bottomRightImage.text = "8"
+    }
+    
+    @IBAction func heartTen(_ sender: AnyObject) {
+        
+        centerImage.text = "10"
+        topLeftImage.text = "10"
+        bottomRightImage.text = "10"
+    }
 }
